74e3963de9daefec7f93098158fba3968d3255d9
Karl <karlsmith@bouzou.com>
2017-02-06 21:46:03 -0500

    Fixes copy-paste errors in setup.py

--- setup.py
+++ setup.py
@@ -17,14 +17,14 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='IntentionDetection',
+    name='nlok',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version='1.0.1',
 
-    description='Detects intentional sentences.',
+    description='Natural Language Object Kit. OO Programming with NLTK.',
     long_description=long_description,
 
     # The project's main homepage.
